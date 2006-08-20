51e86ae7acf669843eeb5eb8a028c081b12c97bb
Ben Bleything <ben@bleything.net>
2006-08-20 23:45:09 +0000

    update tests to reflect Plist::Emit.dump change

--- test/test_generator_basic_types.rb
+++ test/test_generator_basic_types.rb
@@ -10,43 +10,43 @@ require 'plist'
 
 class TestBasicTypes < Test::Unit::TestCase
   def wrap(tag, content)
-    return "<array><#{tag}>#{content}</#{tag}></array>"
+    return "<#{tag}>#{content}</#{tag}>"
   end
 
   def test_strings
     expected = wrap('string', 'testdata')
 
-    assert_equal expected, ['testdata'].to_plist(false)
-    assert_equal expected, [:testdata].to_plist(false)
+    assert_equal expected, Plist::Emit.dump('testdata', false)
+    assert_equal expected, Plist::Emit.dump(:testdata, false)
   end
 
   def test_integers
     [42, 2376239847623987623, -8192].each do |i|
-      assert_equal wrap('integer', i), [i].to_plist(false)
+      assert_equal wrap('integer', i), Plist::Emit.dump(i, false)
     end
   end
 
   def test_floats
     [3.14159, -38.3897, 2398476293847.9823749872349980].each do |i|
-      assert_equal wrap('real', i), [i].to_plist(false)
+      assert_equal wrap('real', i), Plist::Emit.dump(i, false)
     end
   end
 
   def test_booleans
-    assert_equal "<array><true/></array>",  [true].to_plist(false)
-    assert_equal "<array><false/></array>", [false].to_plist(false)
+    assert_equal "<true/>",  Plist::Emit.dump(true, false)
+    assert_equal "<false/>", Plist::Emit.dump(false, false)
   end
 
   def test_time
     test_time = Time.now
-    assert_equal wrap('date', test_time.utc.strftime('%Y-%m-%dT%H:%M:%SZ')), [test_time].to_plist(false)
+    assert_equal wrap('date', test_time.utc.strftime('%Y-%m-%dT%H:%M:%SZ')), Plist::Emit.dump(test_time, false)
   end
 
   def test_dates
     test_date = Date.today
     test_datetime = DateTime.now
 
-    assert_equal wrap('date', test_date.strftime('%Y-%m-%dT%H:%M:%SZ')), [test_date].to_plist(false)
-    assert_equal wrap('date', test_datetime.strftime('%Y-%m-%dT%H:%M:%SZ')), [test_datetime].to_plist(false)
+    assert_equal wrap('date', test_date.strftime('%Y-%m-%dT%H:%M:%SZ')), Plist::Emit.dump(test_date, false)
+    assert_equal wrap('date', test_datetime.strftime('%Y-%m-%dT%H:%M:%SZ')), Plist::Emit.dump(test_datetime, false)
   end
 end
