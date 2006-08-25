08d6554af88f9b26cb9552f5cf76335914ae1a9c
Ben Bleything <ben@bleything.net>
2006-08-25 02:10:28 +0000

    whitespace

--- test/test_generator_collections.rb
+++ test/test_generator_collections.rb
@@ -12,17 +12,17 @@ class TestCollections < Test::Unit::TestCase
   def test_array
     assert_equal "<array><integer>1</integer><integer>2</integer><integer>3</integer></array>", [1,2,3].to_plist(false)
   end
-  
+
   def test_hash
     # only one element because we can't predict what order the keys will come out in.  Sigh.
     assert_equal "<dict><key>foo</key><string>bar</string></dict>", {:foo => :bar}.to_plist(false)
   end
-  
+
   def test_hash_with_array_element
     assert_equal "<dict><key>ary</key><array><integer>1</integer><string>b</string><string>3</string></array></dict>",
                  {:ary => [1,:b,'3']}.to_plist(false)
   end
-  
+
   def test_array_with_hash_element
     assert_equal "<array><dict><key>foo</key><string>bar</string></dict><string>b</string><integer>3</integer></array>",
                  [{:foo => 'bar'}, :b, 3].to_plist(false)
